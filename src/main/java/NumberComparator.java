b5ddcda278b648231f315c16a9f39168606a1363
Jimin <bokjimin143@gmail.com>
2025-09-21 15:05:37 +0900

    Update NumberComparator.java

--- src/main/java/NumberComparator.java
+++ src/main/java/NumberComparator.java
@@ -1,4 +1,15 @@
 public class NumberComparator {
 
+    private int calculateStrike(UserNumber userNumber, ComputerNumber computerNumber) {
+        int strike = 0;
 
+        for (int i = 0; i < 3; i++) {
+
+        }
+    }
+
+
+    public GameResult compare(UserNumber userNumber , ComputerNumber computerNumber) {
+        int strike = calculateStrike(userNumber, computerNumber);
+    }
 }
